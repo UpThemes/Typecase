60be13b108116a4419e716cf8ed68e358bf58061
Anthony Garand <anthonygarand@gmail.com>
2012-04-09 11:46:27 -0400

    Added Compass. Added JS for selectors and no fonts box.

--- main.js
+++ main.js
@@ -1,41 +1,75 @@
-$(document).ready(function(){
+$( document ).ready( function(){
 
-  $("#your-collection").on("click", "a[href='#delete']", $(this), removeFont);
-  $("#available-fonts").on("click", "a[href='#add']", $(this), addFont);
+  $( "#your-collection" ).on( "click", "a[href='#delete']", $( this ), removeFont );
+  $( "#available-fonts" ).on( "click", "a[href='#add']", $( this ), addFont );
+
+  $( "#selectors" ).on( "click", "a[href='#delete']", $( this ), removeSelector );
+  $( "#selectors" ).on( "submit", "#new-selector-form", $( this ), addSelector );
+
+  function addFont( e ) { $( e.target ).trigger( "addFont" );e.preventDefault(); }
+  function removeFont( e ) { $( e.target ).trigger( "removeFont" );e.preventDefault(); }
+
+  function addSelector( e ) { $( e.target ).trigger( "addSelector" );e.preventDefault(); }
+  function removeSelector( e ) { $( e.target ).trigger( "removeSelector" );e.preventDefault(); }
   
-  function addFont(e) {$(e.target).trigger("addFont");e.preventDefault();}
-  function removeFont(e) {$(e.target).trigger("removeFont");e.preventDefault();}
+  $( "#available-fonts" ).bind( "addFont" , function( e ){
   
-  $("#available-fonts").bind("addFont",function(e){
-  
-    if (!$(e.target).closest("a").hasClass("disabled")) {
-      $("#your-collection").find(".font-list").append($(e.target).closest(".font").clone())
-                                      .find(".font:last-child")
-                                      .hide().find(".font-actions")
-                                      .html("<li><a href='#delete'><span></span></a></li>")
-                                      .closest(".font")
+    if ( !$( e.target ).closest( "a" ).hasClass( "disabled" ) ) {
+      $( "#your-collection" ).find( ".no-fonts" ).hide();
+      $( "#your-collection" ).find( ".font-list" ).show().append( $( e.target ).closest( ".font" ).clone() )
+                                      .find( ".font:last-child" )
+                                      .hide().find( ".font-actions" )
+                                      .html( "<li><a href='#delete'><span></span></a></li>" )
+                                      .closest( ".font" )
                                       .slideDown();
-      $(e.target).closest("a").addClass("disabled"); 
+      $( e.target ).closest( "a" ).addClass( "disabled" ); 
     }
     
   });
     
-  $("#your-collection").bind("removeFont",function(e){
+  $( "#your-collection" ).bind( "removeFont" , function( e ){
 
     _this = e.target;
-    $(_this).closest(".font").slideUp(function(){$(_this).remove()});
-    $("#available-fonts .font-list .font."+$(_this).closest(".font").find(".font-sample span").prop("class")+" a[href='#add']").removeClass("disabled");
+    $( _this ).closest( ".font" ).slideUp( function(){ 
+      $( _this ).remove();
+
+      if ( $( "#your-collection .font-list .font" ).length <= 1 ) {
+        $( "#your-collection" ).find( ".font-list" ).hide();
+        $( "#your-collection" ).find( ".no-fonts" ).fadeIn();
+      }
+    });
+    $( "#available-fonts .font-list .font."+$( _this ).closest( ".font" ).find( ".font-sample span" ).prop( "class" )+" a[href='#add']" ).removeClass( "disabled" );
+
+    e.preventDefault();
+
+  });
+
+  $( "#selectors" ).bind( "addSelector" , function( e ){
+
+    _this = e.target;
+    var newSelectorName = $(this).find("#new-selector").val();
+    $(this).find("#new-selector").val("");
+
+    $("<li>" + newSelectorName + "<a href='#delete'><span></span></a></li>").insertBefore("li.add-new").closest("li");
+    $( _this ).closest("ul").find("li:last-child").prev().hide().fadeIn();
+
+    e.preventDefault();
+
+  });
+
+  $( "#selectors" ).bind( "removeSelector" , function( e ){
+
+    _this = e.target;
+    $( _this ).closest( "li" ).fadeOut( function(){ $( _this ).remove() });
     e.preventDefault();
 
   });
 
 });
 
-
-
-google.load("webfont", "1");
+google.load( "webfont", "1" );
   
-google.setOnLoadCallback(function() {
+google.setOnLoadCallback( function() {
   var FontEasy = {
     fontList: false,
     previewText: "The quick brown fox jumps over the lazy dog.",
@@ -47,12 +81,12 @@ google.setOnLoadCallback(function() {
   FontEasy.baseURL = "http://fonts.googleapis.com/css?text="+FontEasy.previewText+"&family=";
   FontEasy.webFontURL = "https://www.googleapis.com/webfonts/v1/webfonts?key="+FontEasy.apiKey+"&sort=alpha&callback=?";
 
-  var getGoogleFonts = function(fontFamilies){
-    $(fontFamilies).each(function(i){
-      var family_class = this.replace(/ /g, '_').toLowerCase();
+  var getGoogleFonts = function( fontFamilies ){
+    $( fontFamilies ).each( function( i ){
+      var family_class = this.replace( / /g, '_' ).toLowerCase();
 
-      if(!$("body").hasClass("wf-"+family_class+"-n4-active")){
-        $('#available-fonts .font-list').append("<div class='font "+family_class+"' rel='."+family_class+"'><style type='text/css'> .font-sample span."+family_class+" { font-family: '"+this+"'; } </style><div class='font-sample'><span class='"+family_class+"'>"+FontEasy.previewText+"</span></div><div class='font-meta'><div class='font-name'>"+this+"</div><ul class='font-actions'><li><a href='#edit'><span></span></a></li><li><a href='#preview'><span></span></a></li><li><a href='#add'><span></span></a></li></ul><!--/.font-actions--></div><!--/.font-meta--><div class='clear'></div></div><!--/.font-->");
+      if( !$( "body" ).hasClass( "wf-"+family_class+"-n4-active" ) ){
+        $( '#available-fonts .font-list' ).append( "<div class='font "+family_class+"' rel='."+family_class+"'><style type='text/css'> .font-sample span."+family_class+" { font-family: '"+this+"'; } </style><div class='font-sample'><span class='"+family_class+"'>"+FontEasy.previewText+"</span></div><div class='font-meta'><div class='font-name'>"+this+"</div><ul class='font-actions'><li><a href='#edit'><span></span></a></li><li><a href='#preview'><span></span></a></li><li><a href='#add'><span></span></a></li></ul><!--/.font-actions--></div><!--/.font-meta--><div class='clear'></div></div><!--/.font-->" );
       }
     });
     
@@ -60,41 +94,41 @@ google.setOnLoadCallback(function() {
   }
 
   
-  var populateFontList = function(data){
-    if(data)
+  var populateFontList = function( data ){
+    if( data )
       FontEasy.fontList = data;
   }
   
-  var loadFonts = function(fontFamilies){
-    position = $(".font-list").scrollTop();
+  var loadFonts = function( fontFamilies ){
+    position = $( ".font-list" ).scrollTop();
 
-    if(!fontFamilies)
+    if( !fontFamilies )
       fontFamilies = FontEasy.fontList;
 
-    if(fontFamilies.kind == "webfonts#webfontList"){
+    if( fontFamilies.kind == "webfonts#webfontList" ){
       var fontFamilies = [];
 
-      $(FontEasy.fontList.items.splice(FontEasy.start,FontEasy.show)).each(function(e){
-        fontFamilies.push(this.family);
+      $( FontEasy.fontList.items.splice( FontEasy.start,FontEasy.show ) ).each( function( e ){
+        fontFamilies.push( this.family );
       });
 
-      getGoogleFonts(fontFamilies);
+      getGoogleFonts( fontFamilies );
     
-      WebFont.load({
+      WebFont.load( {
         google: {
           families: fontFamilies
       }});
     }
   }
 
-  $.getJSON(FontEasy.webFontURL, function(data){
-    populateFontList(data);
+  $.getJSON( FontEasy.webFontURL, function( data ){
+    populateFontList( data );
     loadFonts();
   });
 
-  $("a[href='#more-fonts']").click(function(e){
+  $( "a[href='#more-fonts']" ).click( function( e ){
     loadFonts();
-    $("#available-fonts .font-list").animate({scrollTop:$("#available-fonts .font-list").prop("scrollHeight")});
+    $( "#available-fonts .font-list" ).animate( {scrollTop:$( "#available-fonts .font-list" ).prop( "scrollHeight" )});
     e.preventDefault();
   });
 });
