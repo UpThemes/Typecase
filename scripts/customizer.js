dfdeff8c69e3b2f873b9ffa8d61c3cc31797d11d
ataylorme <andrew@ataylor.me>
2014-12-17 10:24:57 -0600

    Comments and code cleanup

--- scripts/customizer.js
+++ scripts/customizer.js
@@ -1,41 +1,93 @@
 /* globals _wpCustomizeHeader, _wpCustomizeBackground, _wpMediaViewsL10n */
+
+// wait for window load - no iframe ready event (yet)
 jQuery(window).load(function () {
-	
+
+	//	stash the wp.customize object
     var api = wp.customize;
 	
-	function resetAdvancedFonts( api, showAdvanced ){
-		
-		var advancedInputs = jQuery('#accordion-panel-theme_fonts li.control-subsection').not('#accordion-section-theme_fonts_main');
+	// advanced sections are all subsections except the main section
+	var advancedInputs = jQuery('#accordion-panel-theme_fonts li.control-subsection').not('#accordion-section-theme_fonts_main');
 
-        if ( showAdvanced ) {
-            advancedInputs.removeClass('hidden');
-        } else {
-            advancedInputs.addClass('hidden');
-            jQuery('select', advancedInputs).each(function () {
-                var slug = jQuery(this).attr('data-customize-setting-link');
-                api.instance(slug).set('');
-                api.instance(slug).previewer.refresh();
-            });
-        }
-		
+	/*
+	* Function to toggle advanced font options visibility
+	*/
+	function toggleAdvancedFonts( showAdvanced ){
+		if ( showAdvanced ) {
+			
+			// remove the hidden class on advanced sections to make them visible
+			advancedInputs.removeClass('hidden');
+			
+		} else{
+			
+			// add the hidden class on advanced sections to hide them
+			advancedInputs.addClass('hidden');
+			
+		}
 	}
+		
+	/*
+	* Function to reset advanced font options
+	*/
+	function resetAdvancedFonts( showAdvanced ){
 
+		// if show advanced options is false
+		if ( !showAdvanced ) {
+			
+			jQuery('select', advancedInputs).each(function () {
+	
+				// get the unique setting slug
+				var slug = jQuery(this).attr('data-customize-setting-link');
+				
+				// reset the value in customizer 
+				api.instance(slug).set('');
+				
+			});
+			
+		}
+		
+		// refresh the preview
+		api.previewer.refresh();
+
+	}
+	
+	// get the show advanced options value
+	var showAdvanced = api.value('show_advanced_fonts')();
+
+	// reset advanced fonts
+	resetAdvancedFonts( showAdvanced );
+	
+	// when the theme fonts panel link is clicked
     jQuery('#accordion-panel-theme_fonts').click(function () {
+
+		// open the main subsection
         api.section('theme_fonts_main').expand({
+
+			// allow multiple sections to be open at a time
             allowMultiple: true
+
         });
 		
+		// get the show advanced options value
 		var showAdvanced = api.value('show_advanced_fonts')();
 		
-		resetAdvancedFonts( api, showAdvanced );
-        
+		// show/hide advanced fonts based on preference
+		toggleAdvancedFonts( showAdvanced );
+		
     });
 
-
+	// when show advanced fonts option is changed
     api('show_advanced_fonts', function (callback) {
 
+		// bind a callback function to the value
         callback.bind(function ( showAdvanced ) {
-            resetAdvancedFonts( api, showAdvanced );
+
+			// show/hide advanced fonts based on preference
+			toggleAdvancedFonts( showAdvanced );
+			
+			// reset advanced fonts
+			resetAdvancedFonts( showAdvanced );
+
         });
 
     });
